741414b42cba7dced3669cd96b8c835aed8ddad6
JeroenG <jeroengjeroeng@gmail.com>
2016-12-09 21:43:09 +0000

    Apply fixes from StyleCI

--- src/helpers.php
+++ src/helpers.php
@@ -1,15 +1,19 @@
 <?php
-if(! function_exists('isRoute')) {
+
+if (!function_exists('isRoute')) {
 
     /**
      * Determine if the given route is the current one.
-     * 
-     * @param  string  $route
-     * @param  string  $output
-     * @return boolean
+     *
+     * @param string $route
+     * @param string $output
+     *
+     * @return bool
      */
-    function isActive($route, $output = "active")
+    function isActive($route, $output = 'active')
     {
-        if (Route::currentRouteName() == $route) return $output;
+        if (Route::currentRouteName() == $route) {
+            return $output;
+        }
     }
 }
