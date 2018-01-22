60a4d769cdb2f06c599b2da039634668bfd29291
JeroenG <jeroengjeroeng@gmail.com>
2018-01-22 16:38:44 +0100

    added helpers, did some  housekeeping

--- changelog.md
+++ changelog.md
@@ -2,8 +2,15 @@
 
 All Notable changes will be documented in this file.
 
-## Version 1.0
+## Version 1.x
+### Added
+- new page helpers
+- Laravel package autodiscovery
 
+### Removed
+- Cleaned up Displore dependencies for time being
+
+## Version 1.0
 ### Added
 - Custom Blade directives
 - Theming
--- src/helpers.php
+++ src/helpers.php
@@ -1,19 +1,45 @@
 <?php
 
-if (!function_exists('isRoute')) {
-
+if (!function_exists('on_page')) {
     /**
-     * Determine if the given route is the current one.
+     * Determine if the visitor is on the given page.
+     * This uses the URL path.
      *
-     * @param string $route
-     * @param string $output
-     *
+     * @param string $path
      * @return bool
      */
-    function isActive($route, $output = 'active')
+    function on_page($path)
     {
-        if (Route::currentRouteName() == $route) {
-            return $output;
+        return request()->is($path);
+    }
+}
+
+if (!function_exists('on_route')) {
+    /**
+     * Determine if the visitor is on the given page.
+     * This uses the named route.
+     *
+     * @param string $route
+     * @return bool
+     */
+    function on_route($route)
+    {
+        return request()->routeIs($route);
+    }
+}
+
+if (!function_exists('return_if')) {
+    /**
+     * Return the value if the condition is true.
+     *
+     * @param mixed $condition
+     * @param mixed $value
+     * @return void
+     */
+    function return_if($condition, $value)
+    {
+        if ($condition) {
+            return $value;
         }
     }
 }
